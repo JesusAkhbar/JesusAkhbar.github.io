cfd7c453c72d84d588e87402df01272cd839d4e7
Rashev Evgeny <ololosha01@gmail.com>
2022-01-28 16:15:30 +0700

    sda

--- scripts/hz.js
+++
@@ -1,7 +0,0 @@
-var myViewport = document.querySelector("meta[name=viewport]");
-
-    if ($(window).width() < 800) {
-        myViewport.setAttribute("content", "width=800; user-scalable = yes");
-    } else {
-        myViewport.setAttribute("content", "width=device-width; initial-scale = 1.0");
-    }
